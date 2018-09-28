c4fde137aef122527ed42e334709eebb88d8ea95
cWjL <jacobloden@hotmail.com>
2018-09-27 21:15:18 -0700

    changed remote call

--- clean_table.py
+++ clean_table.py
@@ -2,11 +2,12 @@ import csv, os, sys, urllib2
 
 def main(in_file):
     url = "https://www.fjc.gov/sites/default/files/history/judges.csv"
-    cleaned = _get_file_name(in_file)+"_cleaned.csv"
-    _clean_csv(in_file, cleaned)
+    master_csv = in_file
+    cleaned_master = _get_file_name(master_csv)
+    _clean_csv(in_file, cleaned_master)
 
 def _get_remote_csv(url):
-    # Get new csv, save locally
+    # Get new remote csv, save locally
     with open('lists/new_judges.csv','wb') as f:
         f.write(urllib2.urlopen(url).read())
 
@@ -15,7 +16,7 @@ def _get_local_csv(file_path):
 
 def _get_file_name(file_path):
     # Remove file extension
-    return os.path.splitext(file_path)[0]
+    return os.path.splitext(file_path)[0]+"_cleaned.csv"
 
 def _clean_csv(in_file, out_file):
     with open(in_file, 'rb') as inp, open(out_file, 'wb') as out:
