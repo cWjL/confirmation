d2898a6a1db6eeb9a54279e24d2a780be0c1b283
cWjL <jacobloden@hotmail.com>
2018-09-27 21:00:48 -0700

    added remote file grab

--- clean_table.py
+++ clean_table.py
@@ -1,9 +1,17 @@
-import csv, os, sys
+import csv, os, sys, urllib2
 
 def main(in_file):
+    url = "https://www.fjc.gov/sites/default/files/history/judges.csv"
     cleaned = _get_file_name(in_file)+"_cleaned.csv"
     _clean_csv(in_file, cleaned)
 
+def _get_remote_csv(url):
+    # Get new csv, save locally
+    with open('lists/new_judges.csv','wb') as f:
+        f.write(urllib2.urlopen(url).read())
+
+def _get_local_csv(file_path):
+    return csv.reader(file_path)
 
 def _get_file_name(file_path):
     # Remove file extension
