4e6ce2b27aa836703a28bc075971110437d889f8
cWjL <jacobloden@hotmail.com>
2018-10-03 11:23:50 -0700

    added accused

---
+++ accused.py
@@ -0,0 +1,8 @@
+class accused():
+    def __init__(self,first_name,last_name,dob):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.dob = dob
+        
+    def set_dob(self,new_dob):
+        self.dob = new_dob
