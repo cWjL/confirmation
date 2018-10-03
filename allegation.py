4659a7ad19cb7d8a141f7b4fa12334ef4a061208
cWjL <jacobloden@hotmail.com>
2018-10-02 17:18:34 -0700

    changed urls

--- allegation.py
+++ allegation.py
@@ -1,28 +1,24 @@
+#!/usr/bin/env python
 import csv, os, sys, urllib2
 import datetime
 
-def main(in_file, scope):
+def main(scope):
     '''
-    Get list from url, save as judges-MASTER.csv
-
-    Filter list per user parameters
-    
-    Save new list as judges-MASTER_cleaned.csv
-
-    init new _accused class with name, party, hometown, DOB, ... for each name in judges-MASTER_cleaned.csv.  Put in list
-
-    Search for stuff for each _accused in list
-    
+    **********************************************************************************
+    TODO: Create _accused objects
+    TODO: Get filtered csv lists and extract usable stuff
+    TODO: init new _accused class with name, party, hometown, DOB, etc. Put in list
+    TODO: Do the candidate csv stuff
+    TODO: Search for stuff for each _accused in list
+    **********************************************************************************
     '''
     now = datetime.datetime.now()
-    curr_year = now.year
+    curr_year = str(now.year)
     
     jdg_url = "https://www.fjc.gov/sites/default/files/history/judges.csv"
     can_url = "https://www.fec.gov/files/bulk-downloads/"+curr_year+"/candidate_summary_"+curr_year+".csv"
     
-    cleaned_master = _get_file_name(in_file)
-    
-    _clean_judge_csv(in_file, cleaned_master, scope)
+    _clean_judge_csv(_get_remote_csv(jdg_url),scope)
 
 def _get_remote_csv(url):
     # Get new remote csv, return a reader for it
@@ -36,27 +32,40 @@ def _get_file_name(file_path):
     # Remove file extension
     return os.path.splitext(file_path)[0]+"_cleaned.csv"
 
-def _clean_judge_csv(in_file, out_file, scope=None):
+def _clean_judge_csv(in_writer,scope=None):
     '''
-    Change this to take a csv reader and scope only.
+    Remove "Terminated" judges from csv writer passed to it.
 
-    Save the output to lists/<filename>_cleaned.csv
+    Filter either 'r': Replublican, or 'd': Democrat, or '': All judges.
+
+    Write to new csv in lists/ directory.
     '''
-    # Clean out dead judges
-    with open(in_file, 'rb') as inp, open(out_file, 'wb') as out:
+    party = None
+    # get output file label
+    if scope is None:
+        party = "all"
+    elif scope in ( "r","republican","R","Republican"):
+        party = "repub"
+    elif scope in ("d","democrat","D","Democrat","democratic","Democratic"):
+        party = "democrat"
+        
+    out_file = "lists/judges_"+party+".csv"
+
+    # open the output file, filter the csv defined by the csv writer parameter and write it
+    with open(out_file, 'wb') as out:
         writer = csv.writer(out)
         if scope is None:
-            for row in csv.reader(inp):
+            for row in in_writer:
                 if row[43] in (None, "") or row[0] == ("nid"):
                     writer.writerow(row)
         elif scope is "r":
-            for row in csv.reader(inp):
+            for row in in_writer:
                 if (row[43] in (None, "") and row[22] in ("Republican", "None (reassignment)", None, "")) or row[0] == ("nid"):
                     writer.writerow(row)
         elif scope is "d":
-            for row in csv.reader(inp):
+            for row in in_writer:
                 if (row[43] in (None, "") and row[22] in ("Democratic", "Democrat")) or row[0] == ("nid"):
                     writer.writerow(row)
             
 if __name__ == "__main__":
-    main(sys.argv[1], sys.argv[2])
+    main(sys.argv[1])
