5fc591476aac2da19f22a386982db00c1f54817b
cWjL <jacobloden@hotmail.com>
2018-10-04 14:01:02 -0700

    fixed data

--- include/accused.py
+++ include/accused.py
@@ -1,17 +1,8 @@
 class accused():
-    def __init__(self,first_name,last_name):
+        
+    def set_tar_data(self, first_name, last_name, mi=None, dob=None, city=None, state=None, party):
         self.__first_name = first_name
         self.__last_name = last_name
-        self.__mi = None
-        self.__dob = None
-        self.__city = None
-        self.__state = None
-        self.__party = None
-        
-    def set_dob(self,new_dob):
-        self.dob = new_dob
-        
-    def set_tar_data(self, mi=None, dob=None, city=None, state=None, party):
         self.__mi = mi
         self.__dob = dob
         self.__city = city
