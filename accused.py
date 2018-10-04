de93928307e737cee7ea864373f385f07013ee47
cWjL <jacobloden@hotmail.com>
2018-10-04 11:44:05 -0700

    updated

--- accused.py
+++ accused.py
@@ -1,8 +1,10 @@
 class accused():
-    def __init__(self,first_name,last_name,dob):
+    def __init__(self,first_name,last_name,dob=None,city,state):
         self.first_name = first_name
         self.last_name = last_name
         self.dob = dob
+        self.city = city
+        self.state = state
         
     def set_dob(self,new_dob):
         self.dob = new_dob
