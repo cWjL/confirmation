63ce924065ce4e021b3ab45e86fb74b01f50f7e7
cWjL <jacobloden@hotmail.com>
2018-10-04 13:02:52 -0700

    changed attributes

--- accused.py
+++ accused.py
@@ -1,10 +1,14 @@
 class accused():
-    def __init__(self,first_name,last_name,dob=None,city,state):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.dob = dob
-        self.city = city
-        self.state = state
+    def __init__(self,first_name,last_name):
+        self.__first_name = first_name
+        self.__last_name = last_name
+        self.__mi = None
+        self.__dob = None
+        self.__city = None
+        self.__state = None
+        self.__party = None
         
     def set_dob(self,new_dob):
         self.dob = new_dob
+        
+    def 
